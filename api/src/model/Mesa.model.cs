d5a1d2ce268f2a8caf6cb84875ea498638ee8ffb
aomerge <codenode.ortega@gmail.com>
2024-09-08 00:56:06 -0600

    Add Mesa and Orden model classes

---
+++ api/src/model/Mesa.model.cs
@@ -0,0 +1,11 @@
+using System;
+namespace api.src.model;
+
+public class Tb_HccMesas
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public int Capacidad { get; set; }
+    public DateTime FechaCreacion { get; set; }
+
+}
