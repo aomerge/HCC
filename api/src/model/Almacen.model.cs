9e608de0ced7892bb570994b1e8a706cf84ebe52
aomerge <codenode.ortega@gmail.com>
2024-09-08 21:35:36 -0600

    Refactor model classes and add new ones

--- api/src/model/Almacen.model.cs
+++ api/src/model/Almacen.model.cs
@@ -2,5 +2,12 @@
 namespace api.src.model;
 
 public class tbHccAlmacen{
-    
+    public int Id { get; set; }
+
+    public int alm_cantidad { get; set; }
+
+    public DateTime alm_fecha_actualizacion { get; set; }
+
+    public byte alm_estatus { get; set; }
+
 }
--- api/src/model/AppDbContext.cs
+++ api/src/model/AppDbContext.cs
@@ -3,11 +3,14 @@ namespace api.src.model;
 
 public class AppDbContext : DbContext
 {    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<tbHccOrdenes>().ToTable("tbHccOrdenes");
+        modelBuilder.Entity<tbHccEstatusOrden>().ToTable("tbHccEstatusOrden");
+        modelBuilder.Entity<tbHccOrdenDetalle>().ToTable("tbHccOrdenDetalle");
+        modelBuilder.Entity<tbHccProducto>().ToTable("tbHccProducto");
+    }
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-    }
-    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    // {        
-    //     optionsBuilder.UseSqlServer("Server=localhost,1433;Database=HCC;User Id=sa;Password=TuContraseñaSegura!;TrustServerCertificate=True;");
-    // }
+    }    
 }
